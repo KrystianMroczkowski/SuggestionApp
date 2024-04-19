8ec0b7f1c1b0a99048488350c46a30b091660c13
KrystianMroczkowski <mroczkowski.krystian@wp.pl>
2024-04-19 22:29:36 +0200

    Add project files.

---
+++ SuggestionAppLibrary/DataAccess/IUserData.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SuggestionAppLibrary.DataAccess
+{
+	public interface IUserData
+	{
+		Task CreateUser(UserModel user);
+		Task<UserModel> GetUser(string id);
+		Task<UserModel> GetUserFromAuthentication(string objectId);
+		Task<List<UserModel>> GetUsers();
+		Task UpdateUser(UserModel user);
+	}
+}
